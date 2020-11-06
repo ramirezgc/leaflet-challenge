502713327976ab2b153dec727096803e91f4514c
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-11-05 20:56:20 -0600

    Modified folder structure

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,14 +8,15 @@ let myMap = L.map("mapid", {
 console.log(myMap)
 
 let grayMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "<a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  // attribution: "<a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/light-v10",
   accessToken: API_KEY
 });
-comsole.log(grayMap)
+
+grayMap.addTo(myMap);
 
 d3.json(link, function(data) {
   function styleInfo(feature) {
